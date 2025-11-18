73982017a1cdee34826c25e4670989544a278f91
ruchikaroy <contactrroy.uk@gmail.com>
2025-11-18 16:12:21 +0000

    This commit implements dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "./components/ui/provider";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./components/theme";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
